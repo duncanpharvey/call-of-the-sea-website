a00645da30911daf4c5474c543ee08e6c44d44e8
Duncan Harvey <duncanpharvey@gmail.com>
2020-05-11 18:38:28 -0400

    removes fund preselect and adds stripe card input styling

--- js/bloomerang/custom-bloomerang-update-donation-form.js
+++ js/bloomerang/custom-bloomerang-update-donation-form.js
@@ -1,27 +1,24 @@
 function updateDonationForm() {
-    var frequency = document.getElementById("state");
-    frequency.options[0].remove();
-
-    var fund = document.getElementById("fund");
-    if (fund) {
-        fund.value = 119808;
-        fund.options[0].remove();
-    }
-
     var otherAmount = document.getElementById("other-amount");
     if (otherAmount) {
         otherAmount.placeholder = "0.00";
     }
 
     var state = document.getElementById("state");
-    state.value = "CA";
-    state.options[0].remove();
+    if (state) {
+        state.value = "CA";
+        state.options[0].remove();
+    }
 
     var country = document.getElementById("country");
-    country.options[0].remove();
+    if (country) {
+        country.options[0].remove();
+    }
 
     var frequency = document.getElementById("frequency");
-    frequency.options[0].remove();
+    if (frequency) {
+        frequency.options[0].remove();
+    }
 
     var ids = ["donation-amount", "first-name", "last-name", "email-address", "phone-number", "street-address", "city", "state", "zip-code", "comment"];
 
