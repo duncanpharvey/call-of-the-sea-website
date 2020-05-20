67d38c7cc5ca3941540b4b53a515229dd7971cbb
Duncan Harvey <duncanpharvey@gmail.com>
2020-05-19 21:25:45 -0400

    adds anchor scrolling script

---
+++ js/anchor-scrolling.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".program-lesson-links a").forEach(link => {
+        // add anchor scrolling class to lesson links
+        link.classList.add("fusion-one-page-text-link");
+
+        // listen for clicks on lesson links and open corresponding toggle if lesson link is clicked
+        link.addEventListener("click", () => {
+            var toggle = document.querySelector(link.href.match(/#.*$/) + " a");
+            // toggle class = collapsed, active, or null
+            if (!toggle.classList.contains("active")) {
+                toggle.click();
+            }
+        });
+    });
+});
