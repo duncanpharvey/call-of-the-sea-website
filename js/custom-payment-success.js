b6841d7e4c5009e6c706b53daef984efbf46dad6
Duncan Harvey <duncanpharvey@gmail.com>
2020-05-26 08:48:25 -0400

    payment workflow file updates

--- js/custom-payment-success.js
+++ js/custom-payment-success.js
@@ -1,12 +1,10 @@
 function goBack() {
     var urlParams = new URLSearchParams(window.location.search);
     var paymentOrigin = urlParams.get('payment-origin');
-    console.log(paymentOrigin);
     if (paymentOrigin) {
         window.location.href = window.location.origin + paymentOrigin;
     }
     else {
-        var displayError = document.getElementById('error-message');
-        displayError.textContent = "no page to redirect to!";
+        document.getElementById('error-message').textContent = "There is no page to return to!";
     }
 }
--- js/custom-stripe.js
+++ js/custom-stripe.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     var stripe = Stripe('pk_test_dN3uwiG2EgJb92Epj7EQFSKz');
 
-    window.stripePriceIds.forEach(priceId => {
-        var checkoutButton = document.getElementById(priceId);
+    document.querySelectorAll(".stripe-checkout-button").forEach(checkoutButton => {
         checkoutButton.addEventListener('click', function () {
             stripe.redirectToCheckout({
-                lineItems: [{ price: priceId, quantity: 1 }],
+                lineItems: [{ price: checkoutButton.id, quantity: 1 }],
                 mode: 'payment',
                 successUrl: window.location.origin + '/payment-success/?payment-origin=' + window.location.pathname,
                 cancelUrl: window.location.href,
