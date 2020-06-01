f19c6a10aa653aba2985e8581c2616d3896d2967
Duncan Harvey <duncanpharvey@gmail.com>
2020-06-01 16:16:27 -0400

    adds day duration calculation to zapier script

--- zapier/zapier-disembarking-date.py
+++ zapier/zapier-disembarking-date.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta
 
 boardingDateTime = input['boardingDateTime'] if 'boardingDateTime' in input else ""
-duration = input['duration'] if 'duration' in input else ""
+durationHours = input['durationHours'] if 'durationHours' in input else ""
+durationDays = input['durationDays'] if 'durationDays' in input else ""
 
 try:
-    disembarkingDateTime = datetime.strptime(boardingDateTime, '%Y%m%d%H%M') + timedelta(hours = int(duration))
+    delta = timedelta(hours = int(durationHours)) if durationHours else timedelta(days = int(durationDays))
+    disembarkingDateTime = datetime.strptime(boardingDateTime, '%Y%m%d%H%M') + delta
 
     disembarkingDate = disembarkingDateTime.strftime('%Y-%m-%d')
     disembarkingTime = disembarkingDateTime.strftime('%I:%M %p').lstrip("0").lower()
