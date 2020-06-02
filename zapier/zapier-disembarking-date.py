3274d2da6b62bc96325045aadc35251b14364fca
Duncan Harvey <duncanpharvey@gmail.com>
2020-06-02 10:20:57 -0400

    update zapier script to calculate disembarking date if both duration hours and duration nights are specified

--- zapier/zapier-disembarking-date.py
+++ zapier/zapier-disembarking-date.py
@@ -1,12 +1,19 @@
 from datetime import datetime, timedelta
 
 boardingDateTime = input['boardingDateTime'] if 'boardingDateTime' in input else ""
-durationHours = input['durationHours'] if 'durationHours' in input else ""
-durationDays = input['durationDays'] if 'durationDays' in input else ""
 
 try:
-    delta = timedelta(hours = int(durationHours)) if durationHours else timedelta(days = int(durationDays))
-    disembarkingDateTime = datetime.strptime(boardingDateTime, '%Y%m%d%H%M') + delta
+    durationHours = int(input['durationHours'])
+except:
+    durationHours = 0
+
+try:
+    durationNights = int(input['durationNights'])
+except:
+    durationNights = 0
+
+try:
+    disembarkingDateTime = datetime.strptime(boardingDateTime, '%Y%m%d%H%M') + timedelta(days = durationNights, hours = durationHours)
 
     disembarkingDate = disembarkingDateTime.strftime('%Y-%m-%d')
     disembarkingTime = disembarkingDateTime.strftime('%I:%M %p').lstrip("0").lower()
@@ -14,4 +21,4 @@ except:
     disembarkingDate = ""
     disembarkingTime = ""
 
-output = [{'disembarkingDate': disembarkingDate, 'disembarkingTime': disembarkingTime}]
+output = [{ 'disembarkingDate': disembarkingDate, 'disembarkingTime': disembarkingTime }]
