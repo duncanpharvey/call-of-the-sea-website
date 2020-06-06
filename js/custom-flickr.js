78cb586a320a35ab7371443f66b94d645def9545
Duncan Harvey <duncanpharvey@gmail.com>
2020-06-06 11:11:08 -0400

    updates flickr script and styles

--- js/custom-flickr.js
+++ js/custom-flickr.js
@@ -1,3 +1,7 @@
+/*
+    https://callofthesea.org/photo-gallery/
+*/
+
 // flickr photo sizes - https://www.flickr.com/services/api/misc.urls.html
 const default_size = 'm';
 const api_key = 'd06fdce54f321deaef2c50239191f68c';
@@ -8,9 +12,8 @@ const per_page = '24';
 const user_id = '188642069@N06';
 
 var currentPage;
-var totalPages;
-var allLoaded;
 var timer;
+var totalPages;
 
 function getFlickrAlbums() {
     axios.get(flickr_api_url, {
@@ -66,8 +69,8 @@ function getFlickrAlbumPhotos(albumId, page = 1, append = false) {
         if (response.data.stat != 'ok') { return; }
         currentPage = parseInt(response.data.photoset.page);
         totalPages = parseInt(response.data.photoset.pages);
-        var isfeatured = albumId == '72157714587695061' ? true : false;
-        createFlickrPhotoElements(response.data.photoset.photo, response.data.photoset.owner, isfeatured, append);
+        var featured = albumId == '72157714587695061' ? true : false;
+        createFlickrPhotoElements(response.data.photoset.photo, response.data.photoset.owner, featured, append);
     });
 }
 
@@ -84,7 +87,6 @@ function createFlickrAlbumSelect(photosets) {
     select.innerHTML = options_html;
 
     select.addEventListener('change', function () {
-        allLoaded = false;
         var optionId = this.options[this.selectedIndex].id;
         if (optionId == 'all-photos') { getAllFlickrPhotos() }
         else { getFlickrAlbumPhotos(optionId) }
@@ -94,8 +96,8 @@ function createFlickrAlbumSelect(photosets) {
     document.getElementById('72157714587695061').selected = true; // set featured photo album to be the default selected option
 }
 
-function createFlickrPhotoElements(photos, owner, isfeatured, append = false) {
-    if (isfeatured) { size = 'z'; }
+function createFlickrPhotoElements(photos, owner, featured, append = false) {
+    if (featured) { size = 'z'; }
     else { size = default_size; }
     var photo_container = document.getElementById('flickr-photo-container');
 
@@ -104,7 +106,7 @@ function createFlickrPhotoElements(photos, owner, isfeatured, append = false) {
         var embed_url = `https://farm${photo.farm}.static.flickr.com/${photo.server}/${photo.id}_${photo.secret}_${size}.jpg`;
         var link_url = `https://www.flickr.com/photos/${owner ? owner : photo.owner}/${photo.id}`;
 
-        var photo = `<img class="flickr-photo lazyload${isfeatured ? " isfeatured" : ""}" alt="${photo.title}" src="${embed_url}"></img>`;
+        var photo = `<img class="flickr-photo lazyload${featured ? " featured" : ""}" alt="${photo.title}" src="${embed_url}"></img>`;
         var link = `<a href="${link_url}" target="_blank">${photo}</a>`;
 
         var photo_wrapper = document.createElement('div');
@@ -115,16 +117,10 @@ function createFlickrPhotoElements(photos, owner, isfeatured, append = false) {
 }
 
 function loadMoreFlickrPhotos() {
-    var page = currentPage + 1;
-
-    if (page > totalPages) {
-        allLoaded = true;
-        return;
-    }
-
     var select = document.querySelector('select#flickr-album');
     var optionId = select.options[select.selectedIndex].id;
 
+    var page = currentPage + 1;
     if (optionId == 'all-photos') { getAllFlickrPhotos(page, true) }
     else { getFlickrAlbumPhotos(optionId, page, true) }
 }
@@ -135,11 +131,13 @@ function checkScreenPosition() {
     timer = window.setTimeout(function () {
         var photo_container = document.getElementById('flickr-photo-container');
 
-        // load more Flickr photos when the bottom of window is 200 pixels above the bottom of the photo container
-        if (window._fusionScrollTop + window.innerHeight > photo_container.offsetTop + photo_container.offsetHeight - 200 && !allLoaded) { loadMoreFlickrPhotos(); }
+        // load more Flickr photos when the bottom of window is 400 pixels above the bottom of the photo container
+        if (window._fusionScrollTop + window.innerHeight > photo_container.offsetTop + photo_container.offsetHeight - 400 && currentPage < totalPages) { loadMoreFlickrPhotos(); }
     }, 100);
 }
 
-getFlickrAlbums();
-getFlickrAlbumPhotos('72157714587695061'); // load featured photos by default
-document.addEventListener('scroll', checkScreenPosition); // monitor scroll event for infinite scrolling
+document.addEventListener('DOMContentLoaded', () => {
+    getFlickrAlbums();
+    getFlickrAlbumPhotos('72157714587695061'); // load featured photos by default
+    document.addEventListener('scroll', checkScreenPosition); // monitor scroll event for infinite scrolling
+});
