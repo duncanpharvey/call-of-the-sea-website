43ca9e5804c52eaa36dc8fbb584f2a76236a885c
Duncan Harvey <duncanpharvey@gmail.com>
2020-06-12 13:16:32 -0400

    updates page title action to use id in body class instead of the site title which could be changed

--- js/google-analytics/page-title-action.js
+++ js/google-analytics/page-title-action.js
@@ -1,5 +1,3 @@
 function() {
-    var title = document.title.toLowerCase();
-    var label = title == 'call of the sea - sail, explore, learn' ? 'home' : title.replace(/ - call of the sea/g, '');
-    return 'click ' + label;
+    return 'click ' + (document.body.classList.contains('page-id-757') ? 'home' : document.title.toLowerCase().replace(/ - call of the sea/g, ''));
 }
